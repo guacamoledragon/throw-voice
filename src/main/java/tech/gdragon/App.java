f8cfde1fd46fc3db3a4ef124a409b2e701d9c384
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-07-09 09:21:30 -0600

    Create simple http service that redirects to bot authorization pg

--- src/main/java/tech/gdragon/App.java
+++ src/main/java/tech/gdragon/App.java
@@ -1,15 +1,33 @@
 package tech.gdragon;
 
-public final class App {
-  private App() {
+
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.IOException;
+
+public final class App extends NanoHTTPD {
+
+  private App(int port) {
+    super(port);
+  }
+
+  @Override
+  public Response serve(IHTTPSession session) {
+    String botUrl = "https://discordapp.com/oauth2/authorize?client_id=333132997245272067&scope=bot&permissions=36703232";
+    Response response = newFixedLengthResponse(Response.Status.REDIRECT, NanoHTTPD.MIME_HTML, "");
+    response.addHeader("Location", botUrl);
+    return response;
   }
 
   /**
-   * Says hello to the world.
-   *
-   * @param args The arguments of the program.
+   * Starts a simple HTTP Service, whose only response is to redirect to the bot's page.
    */
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    App app = new App(8080);
+    try {
+      app.start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
