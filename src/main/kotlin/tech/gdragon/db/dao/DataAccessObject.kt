af5653ef56332f62ef3b2dcb20e5105a811281b3
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-08-12 09:36:29 -0600

    Implement DAO for all tables

--- src/main/kotlin/tech/gdragon/db/dao/DataAccessObject.kt
+++ src/main/kotlin/tech/gdragon/db/dao/DataAccessObject.kt
@@ -2,10 +2,26 @@ package tech.gdragon.db.dao
 
 import org.jetbrains.exposed.dao.*
 import tech.gdragon.db.table.Tables.Aliases
+import tech.gdragon.db.table.Tables.Channels
 import tech.gdragon.db.table.Tables.Guilds
+import tech.gdragon.db.table.Tables.Users
 import tech.gdragon.db.table.Tables.SettingsAliases
 import tech.gdragon.db.table.Tables.Settings as SettingsTable
 
+class Alias(id: EntityID<Int>) : IntEntity(id) {
+  companion object : IntEntityClass<Alias>(Aliases)
+
+  var name by Aliases.name
+  var alias by Aliases.alias
+}
+
+class Channel(id: EntityID<Int>) : IntEntity(id) {
+  companion object : IntEntityClass<Channel>(Channels)
+
+  var name by Channels.name
+  var autojoin by Channels.autojoin
+  var autoleave by Channels.autoleave
+}
 
 class Guild(id: EntityID<Long>) : LongEntity(id) {
   companion object : LongEntityClass<Guild>(Guilds)
@@ -18,12 +34,17 @@ class Settings(id: EntityID<Int>) : IntEntity(id) {
   companion object : IntEntityClass<Settings>(SettingsTable)
 
   var autoSave by SettingsTable.autoSave
+  var defaultTextChannel by SettingsTable.defaultTextChannel
+  var prefix by SettingsTable.prefix
+  var volume by SettingsTable.volume
+
+  var alertBlacklist by User via Users
+  var channels by Channel via Channelsa,
   var aliases by Alias via SettingsAliases
 }
 
-class Alias(id: EntityID<Int>) : IntEntity(id) {
-  companion object : IntEntityClass<Alias>(Aliases)
+class User(id: EntityID<Int>) : IntEntity(id) {
+  companion object : IntEntityClass<User>(Users)
 
-  var name by Aliases.name
-  var alias by Aliases.alias
+  var name by Users.name
 }
