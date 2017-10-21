fc3c3ee4f20d4d77871db5b96ec7e38e9b1ca7a5
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-10-20 21:41:25 -0600

    Deprecate VolumeCommand

--- src/main/java/tech/gdragon/commands/settings/VolumeCommand.java
+++ src/main/java/tech/gdragon/commands/settings/VolumeCommand.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import tech.gdragon.DiscordBot;
 import tech.gdragon.commands.Command;
 
-
+@Deprecated
 public class VolumeCommand implements Command {
   @Override
   public void action(String[] args, GuildMessageReceivedEvent e) {
