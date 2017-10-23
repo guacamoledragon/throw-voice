683bccdea7a2de59b2247dbba8233feff6f448ad
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-10-22 21:20:14 -0600

    Deprecate ServerSettings to make it easier to remove

--- src/main/java/tech/gdragon/configuration/ServerSettings.java
+++ src/main/java/tech/gdragon/configuration/ServerSettings.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.core.entities.VoiceChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+@Deprecated
 public class ServerSettings {
 
   public HashMap<String, Integer> autoJoinSettings;
