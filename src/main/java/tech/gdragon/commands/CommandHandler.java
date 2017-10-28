d8616e47f12e2f02bbf34b1a10616bfa1bfd3b7a
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-10-28 11:40:22 -0600

    Overload handleCommand, uses database shim and accepts guild event

--- src/main/java/tech/gdragon/commands/CommandHandler.java
+++ src/main/java/tech/gdragon/commands/CommandHandler.java
@@ -1,9 +1,15 @@
 package tech.gdragon.commands;
 
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import tech.gdragon.DiscordBot;
 import tech.gdragon.configuration.ServerSettings;
+import tech.gdragon.db.Shim;
+import tech.gdragon.db.dao.Alias;
+import tech.gdragon.db.dao.Guild;
+import tech.gdragon.db.dao.Settings;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class CommandHandler {
   public static final CommandParser parser = new CommandParser();
@@ -24,5 +30,29 @@ public class CommandHandler {
 
       commands.get(invoke).action(cmd.args, cmd.event);
     }
+  }
+
+  public static boolean handleCommand(GuildMessageReceivedEvent event, CommandParser.CommandContainer commandContainer) {
+    return
+      Shim.INSTANCE.xaction(() -> {
+        Boolean isSuccess = false;
+        Settings ss = Guild.Companion.findById(event.getGuild().getIdLong()).getSettings();
+        String command = commandContainer.invoke;
+
+        if (!commands.containsKey(command)) {
+          for (Alias alias : ss.getAliases()) {
+            if (alias.getAlias().equals(command)) {
+              commands.get(alias.getName()).action(commandContainer.args, event);
+              isSuccess = true;
+              break;
+            }
+          }
+        } else {
+          commands.get(command).action(commandContainer.args, event);
+          isSuccess = true;
+        }
+
+        return isSuccess;
+      });
   }
 }
