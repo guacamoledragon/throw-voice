7c06190e4ce7394d7ebd27b84b57d3cbba7f5bab
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-11-26 23:37:43 -0700

    Break down audio tester into logical segments for ad-hoc tests

--- src/main/kotlin/tech/gdragon/AudioListenerApp.kt
+++ src/main/kotlin/tech/gdragon/AudioListenerApp.kt
@@ -4,12 +4,20 @@ import net.dv8tion.jda.core.audio.CombinedAudio
 import net.dv8tion.jda.core.entities.impl.GuildImpl
 import net.dv8tion.jda.core.entities.impl.UserImpl
 import net.dv8tion.jda.core.entities.impl.VoiceChannelImpl
+import tech.gdragon.listener.CombinedAudioRecorderHandler
 import tech.gdragon.listeners.AudioReceiveListener
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.nio.ByteBuffer
 
 fun main(args: Array<String>) {
+  val audioSegments = generateAudioSegments()
+
+//  legacyHandler(audioSegments)
+  handler(audioSegments)
+}
+
+private fun generateAudioSegments(): List<CombinedAudio> {
   val combinedAudioSegments = mutableListOf<CombinedAudio>()
 
   // Recreate combined audio segment stream
@@ -28,17 +36,40 @@ fun main(args: Array<String>) {
     }
   }
 
+  return combinedAudioSegments.toList()
+}
+
+private fun legacyHandler(audioSegments: List<CombinedAudio>) {
   val audioReceiveListener = AudioReceiveListener(0.8, VoiceChannelImpl(1L, GuildImpl(null, 1L)))
 
-  combinedAudioSegments.forEach(audioReceiveListener::handleCombinedAudio)
+  audioSegments.forEach(audioReceiveListener::handleCombinedAudio)
 
-  FileOutputStream("recordings/alone-out.pcm").use { pcm ->
+  FileOutputStream("recordings/legacy-alone-out.pcm").use { pcm ->
     val rawVoiceData = audioReceiveListener.getUncompVoice((AudioReceiveListener.PCM_MINS * 60).toInt())
     pcm.write(rawVoiceData)
 
-    FileOutputStream("recordings/alone-out.mp3").use { mp3 ->
+    FileOutputStream("recordings/legacy-alone-out.mp3").use { mp3 ->
       val voiceData = BotUtils.encodePcmToMp3(rawVoiceData)
       mp3.write(voiceData)
     }
   }
+}
+
+private fun handler(audioSegments: List<CombinedAudio>) {
+  val audioReceiveListener = CombinedAudioRecorderHandler(0.8, VoiceChannelImpl(1L, GuildImpl(null, 1L)).also { it.name = "alone-out" })
+  audioSegments.forEach(audioReceiveListener::handleCombinedAudio)
+
+  audioReceiveListener
+    .getVoiceData()
+    ?.subscribe { baos ->
+
+      FileOutputStream("recordings/alone-out.mp3").use { mp3 ->
+        val rawVoiceData = baos.toByteArray()
+        baos.reset()
+        baos.close()
+        val voiceData = BotUtils.encodePcmToMp3(rawVoiceData)
+
+        mp3.write(voiceData)
+      }
+    }
 }
