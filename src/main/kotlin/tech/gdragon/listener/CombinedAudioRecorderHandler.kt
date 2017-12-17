a9f814955af8c52d11e84f34587a829f5fae2858
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-12-17 15:18:34 -0700

    Move observable chain into a method, createRecording

--- src/main/kotlin/tech/gdragon/listener/CombinedAudioRecorderHandler.kt
+++ src/main/kotlin/tech/gdragon/listener/CombinedAudioRecorderHandler.kt
@@ -1,5 +1,6 @@
 package tech.gdragon.listener
 
+import com.squareup.tape.QueueFile
 import de.sciss.jump3r.lowlevel.LameEncoder
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
@@ -13,66 +14,42 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
 import tech.gdragon.BotUtils
 import tech.gdragon.db.dao.Guild
-import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import java.io.RandomAccessFile
-import java.nio.channels.Channels
-import java.nio.channels.FileChannel
+import java.io.File
+import java.io.FileOutputStream
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 
-class CombinedAudioRecorderHandler(val volume: Double, val voiceChannel: VoiceChannel, val uuid: UUID = UUID.randomUUID()) : AudioReceiveHandler {
+class CombinedAudioRecorderHandler(val volume: Double, val voiceChannel: VoiceChannel) : AudioReceiveHandler {
   companion object {
-    private const val BYTE_SAMPLE_SIZE = 3840 // Size of one 20 ms sample
-    private const val AFK_LIMIT = (2 * 60 * 1000) / 20 // 2 mins in ms over 20ms increments
+    private const val AFK_LIMIT = (2 * 60 * 1000) / 20      // 2 minutes in ms over 20ms increments
+    private const val MAX_RECORDING_SIZE =  1 * 1024 * 1024 // 8MB
+    private const val BUFFER_TIMEOUT = 200L                 // 200 milliseconds
+    private const val BUFFER_MAX_COUNT = 8
   }
 
   private val logger = LoggerFactory.getLogger(this.javaClass)
 
-  val mp3Filename = "recordings/$uuid.mp3"
-  val mp3Channel: FileChannel? = RandomAccessFile(mp3Filename, "rw").channel
-
-  private val subject: Subject<CombinedAudio> = PublishSubject.create()
+  // State-licious
+  private var subject: Subject<CombinedAudio>? = null
   private var subscription: Disposable? = null
+  private var uuid: UUID? = null
 
   private var canReceive = true
   private var afkCounter = 0
 
+  var mp3Filename: String? = null
+
   init {
-    val encoder = LameEncoder(AudioReceiveHandler.OUTPUT_FORMAT, 128, LameEncoder.CHANNEL_MODE_AUTO, LameEncoder.QUALITY_HIGHEST, false)
-
-    subscription = subject
-      .map { it.getAudioData(volume) }
-      .buffer(200, TimeUnit.MILLISECONDS, 8)
-      .flatMap({ bytesArray ->
-        val baos = ByteArrayOutputStream()
-
-        bytesArray.forEach {
-          val buffer = ByteArray(it.size)
-          val bytesEncoded = encoder.encodeBuffer(it, 0, it.size, buffer)
-          baos.write(buffer, 0, bytesEncoded)
-        }
-
-        Observable.fromArray(baos.toByteArray())
-      })
-      .collectInto(mp3Channel, { channel, bytes ->
-        val inputByteChannel = Channels.newChannel(ByteArrayInputStream(bytes))
-
-        channel?.apply {
-          val size = transferFrom(inputByteChannel, position(), bytes.size.toLong())
-          position(position() + size)
-        }
-      })
-      .map { println("it.size() = ${it.size()}") }
-      .subscribe()
+    subscription = createRecording()
   }
 
   override fun canReceiveUser(): Boolean = false
 
   override fun handleCombinedAudio(combinedAudio: CombinedAudio) {
     if (!isAfk(combinedAudio.users.size)) {
-      subject.onNext(combinedAudio)
+      subject?.onNext(combinedAudio)
     }
   }
 
@@ -106,10 +83,53 @@ class CombinedAudioRecorderHandler(val volume: Double, val voiceChannel: VoiceCh
     return isAfk
   }
 
+  private fun createRecording(): Disposable? {
+    subject = PublishSubject.create()
+    uuid = UUID.randomUUID()
+    mp3Filename = "recordings/$uuid.mp3"
+
+    var recordingSize = 0
+
+    val encoder = LameEncoder(AudioReceiveHandler.OUTPUT_FORMAT, 128, LameEncoder.CHANNEL_MODE_AUTO, LameEncoder.QUALITY_HIGHEST, false)
+
+    return subject
+      ?.map { it.getAudioData(volume) }
+      ?.buffer(BUFFER_TIMEOUT, TimeUnit.MILLISECONDS, BUFFER_MAX_COUNT)
+      ?.flatMap({ bytesArray ->
+        val baos = ByteArrayOutputStream()
+
+        bytesArray.forEach {
+          val buffer = ByteArray(it.size)
+          val bytesEncoded = encoder.encodeBuffer(it, 0, it.size, buffer)
+          baos.write(buffer, 0, bytesEncoded)
+        }
+
+        Observable.fromArray(baos.toByteArray())
+      })
+      ?.collectInto(QueueFile(File("recordings/$uuid.queue")), { queue, bytes ->
+
+        while (recordingSize + bytes.size > MAX_RECORDING_SIZE) {
+          recordingSize -= queue.peek()?.size ?: 0
+          queue.remove()
+        }
+
+        queue.add(bytes)
+        recordingSize += bytes.size
+      })
+      ?.subscribe({ queue ->
+        val outputStream = FileOutputStream(File(mp3Filename))
+        queue.forEach({ stream, _ -> stream.transferTo(outputStream) })
+        queue.close()
+        outputStream.close()
+        File("recordings/$uuid.queue").delete()
+
+        println("queue.size() = ${queue.size()}")
+      })
+  }
+
   fun disconnect() {
     canReceive = false
-    subject.onComplete()
+    subject?.onComplete()
     subscription?.dispose()
-    mp3Channel?.close()
   }
 }
