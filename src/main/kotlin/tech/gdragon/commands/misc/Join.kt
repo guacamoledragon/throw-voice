b5fcf714a8dfbc87c8eec3507d4931f995def47e
Jose V. Trigueros <j.v.trigueros@gmail.com>
2018-02-12 08:00:32 -0700

    Add InvalidCommand to misc commands

--- src/main/kotlin/tech/gdragon/commands/misc/Join.kt
+++ src/main/kotlin/tech/gdragon/commands/misc/Join.kt
@@ -4,30 +4,30 @@ import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
 import org.jetbrains.exposed.sql.transactions.transaction
 import tech.gdragon.BotUtils
 import tech.gdragon.commands.Command
+import tech.gdragon.commands.InvalidCommand
 import tech.gdragon.db.dao.Guild
 import tech.gdragon.listener.CombinedAudioRecorderHandler
 
 class Join : Command {
   override fun action(args: Array<String>, event: GuildMessageReceivedEvent) {
+    require(args.isEmpty()) {
+      throw InvalidCommand(::usage, "Incorrect number of arguments: ${args.size}")
+    }
+
     transaction {
       val guild = Guild.findById(event.guild.idLong)
-      val prefix = guild?.settings?.prefix ?: "!"
-
-      require(args.isEmpty()) {
-        BotUtils.sendMessage(event.channel, usage(prefix))
-      }
 
       val voiceChannel = event.member.voiceState.channel
       val message =
-        if(voiceChannel == null) {
+        if (voiceChannel == null) {
           ":no_entry_sign: _Please join a voice channel before using this command._"
         } else {
           val connectedChannel = event.guild.audioManager.connectedChannel
-          if(connectedChannel != null && connectedChannel.members.contains(event.member)) {
+          if (connectedChannel != null && connectedChannel.members.contains(event.member)) {
             ":no_entry_sign: _I am already in **<#${connectedChannel.id}>**._"
           } else {
 
-            if(event.guild.audioManager.isConnected) {
+            if (event.guild.audioManager.isConnected) {
               guild?.settings?.let {
                 if (it.autoSave) {
                   val audioReceiveHandler = event.guild.audioManager.receiveHandler as CombinedAudioRecorderHandler
