0f735a2e43df775c7a0cc36ee372cbe63619f93b
Jose V. Trigueros <j.v.trigueros@gmail.com>
2018-02-23 21:44:49 -0700

    Add start script for Windows users

---
+++ src/assembly/start.bat
@@ -0,0 +1,28 @@
+@echo off
+
+REM Bot specific variables, required
+SET BOT_TOKEN=
+SET CLIENT_ID=
+SET DATA_DIR=
+SET PORT=
+
+REM Storage specific variables, required
+SET B2_APP_KEY=
+SET B2_ACCOUNT_ID=
+SET B2_BASE_URL=
+SET B2_BUCKET_ID=
+SET B2_BUCKET_NAME=
+
+REM Logging specific variables, optional
+SET ROLLBAR_ENV=
+SET ROLLBAR_TOKEN=
+
+@echo.
+echo ============================== Starting Bot ==============================
+echo    Version:  ${version}
+echo Build Date:  ${timestamp}
+echo   Revision:  https://github.com/guacamoledragon/throw-voice/commit/${revision}
+echo ==========================================================================
+@echo.
+
+java -cp ${name}-${version}.jar;lib\* tech.gdragon.App
