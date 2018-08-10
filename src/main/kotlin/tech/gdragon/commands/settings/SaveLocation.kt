20a25359dd0d31fa4655bf2965e49254aa75a521
Jose V. Trigueros <j.v.trigueros@gmail.com>
2018-08-09 21:24:58 -0600

    Dry this up a bit by creating a fn that actually sets the db

--- src/main/kotlin/tech/gdragon/commands/settings/SaveLocation.kt
+++ src/main/kotlin/tech/gdragon/commands/settings/SaveLocation.kt
@@ -1,13 +1,24 @@
 package tech.gdragon.commands.settings
 
+import net.dv8tion.jda.core.entities.TextChannel
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
 import org.jetbrains.exposed.sql.transactions.transaction
 import tech.gdragon.BotUtils
 import tech.gdragon.commands.Command
 import tech.gdragon.commands.InvalidCommand
 import tech.gdragon.db.dao.Guild
+import tech.gdragon.db.dao.Settings
 
 class SaveLocation : Command {
+  private fun setSaveLocation(settings: Settings, channel: TextChannel): String {
+    return if (channel.canTalk()) {
+      settings.defaultTextChannel = channel.idLong
+      ":file_folder: _All messages will default to channel **<#${channel.id}>**._"
+    } else {
+      ":no_entry_sign: _Cannot send messages in **<#${channel.id}>**, please configure permissions and try again._"
+    }
+  }
+
   override fun action(args: Array<String>, event: GuildMessageReceivedEvent) {
     require(args.size in 0..1) {
       throw InvalidCommand(::usage, "Incorrect number of arguments: ${args.size}")
@@ -19,12 +30,7 @@ class SaveLocation : Command {
       val message =
         guild?.settings?.let {
           if (args.isEmpty()) {
-            if (event.channel.canTalk()) {
-              it.defaultTextChannel = event.channel.idLong
-              ":file_folder: _All messages will default to channel **<#${event.channel.id}>**._"
-            } else {
-              ":no_entry_sign: _Cannot send messages in **<#${event.channel.id}>**, please configure permissions and try again._"
-            }
+            setSaveLocation(it, event.channel)
           } else {
             val channelName = if (args.first().startsWith("#")) args.first().substring(1) else args.first()
             val channels = event.guild.getTextChannelsByName(channelName, true)
@@ -32,13 +38,7 @@ class SaveLocation : Command {
             if (channels.isEmpty()) {
               ":no_entry_sign: _Cannot find text channel **${args.first()}**!_"
             } else {
-              val channel = channels.first()
-              if (channel.canTalk()) {
-                it.defaultTextChannel = channel.idLong
-                ":file_folder: _All messages will default to channel **<#${channel.id}>**._"
-              } else {
-                ":no_entry_sign: _Cannot send messages in **<#${channel.id}>**, please configure permissions and try again._"
-              }
+              setSaveLocation(it, channels.first())
             }
           }
         } ?: ":no_entry_sign: _Could not set default save location._"
