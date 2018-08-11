8a7b74505c737d576f5eb00e9f9871e9deb02b64
Jose V. Trigueros <j.v.trigueros@gmail.com>
2018-08-11 10:58:14 -0600

    DMs sent to bot will respond with URL

--- src/main/kotlin/tech/gdragon/commands/settings/Alerts.kt
+++ src/main/kotlin/tech/gdragon/commands/settings/Alerts.kt
@@ -1,28 +1,14 @@
 package tech.gdragon.commands.settings
 
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
-import org.jetbrains.exposed.sql.transactions.transaction
 import tech.gdragon.BotUtils
 import tech.gdragon.commands.Command
-import tech.gdragon.db.dao.Guild
-import tech.gdragon.db.dao.User
-
-fun configureAlerts(userId: String, guildId: Long, enable: Boolean) {
-  transaction {
-    val settings = Guild.findById(guildId)?.settings!!
-    val user = User.findOrCreate(userId, settings)
-
-    if (enable) {
-      user.delete()
-    }
-  }
-}
 
 class Alerts : Command {
 
   override fun action(args: Array<String>, event: GuildMessageReceivedEvent) {
     val botId = event.jda.selfUser.id
-    val message = ":no_entry_sign: _<@$botId> will no longer send alerts, this command will be removed in the next major release._"
+    val message = ":no_entry_sign: _<@$botId> will no longer send alerts, this command will be removed in the next release._"
     BotUtils.sendMessage(event.channel, message)
   }
 
