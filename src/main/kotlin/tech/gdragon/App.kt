6cf36f50b8546814ef3aa3de5ab5cb2cf8ae0143
Jose V. Trigueros <j.v.trigueros@gmail.com>
2018-11-23 13:56:18 -0800

    Initialize config object

--- src/main/kotlin/tech/gdragon/App.kt
+++ src/main/kotlin/tech/gdragon/App.kt
@@ -1,15 +1,18 @@
 package tech.gdragon
 
+import com.natpryce.konfig.ConfigurationProperties
+import com.natpryce.konfig.EnvironmentVariables
+import com.natpryce.konfig.overriding
 import fi.iki.elonen.NanoHTTPD
 import mu.KotlinLogging
 import tech.gdragon.db.Shim
-import tech.gdragon.discord.Bot
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.time.Duration
 import java.util.*
 import kotlin.concurrent.scheduleAtFixedRate
+import tech.gdragon.discord.Bot as DiscordBot
 
 class App private constructor(port: Int, val clientId: String, val inviteUrl: String) : NanoHTTPD(port) {
   override fun serve(session: NanoHTTPD.IHTTPSession): NanoHTTPD.Response {
@@ -37,6 +40,10 @@ class App private constructor(port: Int, val clientId: String, val inviteUrl: St
      */
     @JvmStatic
     fun main(args: Array<String>) {
+      val config = ConfigurationProperties.systemProperties() overriding
+        EnvironmentVariables() overriding
+        ConfigurationProperties.fromResource("defaults.properties")
+
       val token = System.getenv("BOT_TOKEN")
       val port = System.getenv("PORT")
       val clientId = System.getenv("CLIENT_ID") // TODO: Can we get rid of this w/o consequences?
@@ -45,8 +52,8 @@ class App private constructor(port: Int, val clientId: String, val inviteUrl: St
       // Connect to database
       Shim.initializeDatabase("$dataDirectory/settings.db")
 
-      val bot = Bot(token)
-      val inviteUrl = bot.api.asBot().getInviteUrl(Bot.PERMISSIONS)
+      val bot = DiscordBot(token)
+      val inviteUrl = bot.api.asBot().getInviteUrl(DiscordBot.PERMISSIONS)
       val app = App(Integer.parseInt(port), clientId, inviteUrl)
 
       try {
