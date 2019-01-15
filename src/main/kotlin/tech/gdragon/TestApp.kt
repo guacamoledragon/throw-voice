8fc0eca44eb10df4d9624f8226acd57e6f08bc8c
Jose V. Trigueros <j.v.trigueros@gmail.com>
2019-01-14 18:52:54 -0800

    Add DataStore class to handle permanent data interactions

--- src/main/kotlin/tech/gdragon/TestApp.kt
+++ src/main/kotlin/tech/gdragon/TestApp.kt
@@ -1,11 +1,15 @@
 package tech.gdragon
 
+import io.minio.MinioClient
 import net.dv8tion.jda.core.AccountType
 import net.dv8tion.jda.core.JDABuilder
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.not
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
@@ -135,5 +139,21 @@ fun main(args: Array<String>) {
 //  dropAllTables()
 //  uploadRecording()
 //  testAutoJoin()
-  removeUnusedGuilds()
+//  removeUnusedGuilds()
+  minio()
+}
+
+fun minio() {
+  val minioClient = MinioClient("http://localhost:9000", System.getenv("B2_ACCOUNT_ID"), System.getenv("B2_APP_KEY"))
+  minioClient.listBuckets().forEach { bucket ->
+    println("bucket = ${bucket.name()}")
+  }
+
+  val objectUrl = minioClient.getObjectUrl("dev-recordings", "/333055724198559745/0245a36c-654a-4b3b-8718-4e9d99f21fc3.mp3")
+  println("recordingObject = $objectUrl")
+
+//  minioClient.putObject("dev-recordings", "/333055724198559745/4ceeafa2-17ac-4d5f-9a7b-9903c6f11fa8.mp3", "./data/test-data/mp3-encoded/4ceeafa2-17ac-4d5f-9a7b-9903c6f11fa8.mp3")
+
+  val statObject = minioClient.getObjectUrl("dev-recordings", "/333055724198559745/4ceeafa2-17ac-4d5f-9a7b-9903c6f11fa8.mp3")
+  println("statObject = $statObject")
 }
---
+++ src/main/kotlin/tech/gdragon/data/DataStore.kt
@@ -0,0 +1,4 @@
+package tech.gdragon.data
+
+class DataStore {
+}
