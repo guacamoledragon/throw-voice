242c4c93fa46a7cac390e36f7d3af86cf163a12e
Jose V. Trigueros <j.v.trigueros@gmail.com>
2019-04-02 21:33:18 -0700

    Add migration to fix database schema and nullable autostop

--- sql/V20190331160406__make-autostop-nullable.sql
+++ sql/V20190331160406__make-autostop-nullable.sql
@@ -1,2 +1,33 @@
--- TODO: Remove all channel rows that autoRecord = null and autoStop = 1
--- TODO: Allow autostop to be null, just like autorecord
+-- Remove all channel rows that autoRecord = null and autoStop = 1
+delete
+from Channels
+where autoStop is 1
+  and autoRecord isnull;
+
+-- Reset autoStop to null to prevent accidental leaves
+update Channels
+set autoStop = null
+where autoStop is 1;
+
+--  Allow autostop to be null, just like autorecord
+create table Channels_dg_tmp
+(
+  id         INTEGER
+    primary key,
+  name       TEXT   not null,
+  autoRecord INT,
+  autoStop   INT,
+  settings   BIGINT not null
+    references Settings
+      on delete cascade
+);
+
+insert into Channels_dg_tmp(id, name, autoRecord, autoStop, settings)
+select id, name, autoRecord, autoStop, settings
+from Channels;
+
+drop table Channels;
+
+alter table Channels_dg_tmp
+  rename to Channels;
+
