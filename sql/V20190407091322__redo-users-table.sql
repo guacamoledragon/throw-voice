9732e70fa95d6b943278c017a03f1a9aa2c14e3a
Jose V. Trigueros <j.v.trigueros@gmail.com>
2019-04-07 10:03:11 -0700

    Update migration, Guilds should know who activated it

--- sql/V20190407091322__redo-users-table.sql
+++ sql/V20190407091322__redo-users-table.sql
@@ -2,9 +2,30 @@ drop table Users;
 
 create table Users
 (
-  id              INTEGER primary key,
-  name            TEXT not null,
-  voted_on        TEXT,
-  supporter       TEXT,
-  activated_guild BIGINT references Guilds
+  id        INTEGER primary key,
+  name      TEXT not null,
+  voted_on  TEXT,
+  supporter TEXT
 );
+
+create table Guilds_dg_tmp
+(
+  id             INTEGER
+    primary key,
+  name           TEXT not null,
+  region         TEXT,
+  created_on     TEXT,
+  last_active_on TEXT,
+  activated_by   INTEGER
+    constraint Guilds_Users_id_fk
+      references Users
+);
+
+insert into Guilds_dg_tmp(id, name, region, created_on, last_active_on)
+select id, name, region, created_on, last_active_on
+from Guilds;
+
+drop table Guilds;
+
+alter table Guilds_dg_tmp
+  rename to Guilds;
