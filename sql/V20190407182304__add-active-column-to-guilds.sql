7361cc58752eb43f76252cccd178403e82d6c3f0
Jose V. Trigueros <j.v.trigueros@gmail.com>
2019-04-08 20:28:24 -0700

    Mark as inactive instead of deleting

--- sql/V20190407182304__add-active-column-to-guilds.sql
+++ sql/V20190407182304__add-active-column-to-guilds.sql
@@ -1,8 +1,7 @@
--- Remove Guilds that never connected since the new year
-delete
-from Guilds
+alter table Guilds
+  add active INTEGER default 1;
+
+update Guilds
+set active = 0
 where created_on = '2019-01-01 00:00:00'
   and last_active_on = '2019-01-01 00:00:00';
-
-alter table Guilds
-  add active TEXT default 'true';
