08a1aca2c97dcc5b0e46676a5ea83189cd762df2
Jose V. Trigueros <j.v.trigueros@gmail.com>
2019-05-28 17:50:18 -0700

    Add start.sh for Linux users

---
+++ src/assembly/start.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# Bot variables, required
+BOT_TOKEN=
+DATA_DIR=.
+DISCORD_WEBHOOK=
+PORT=
+
+# Minio Cloud Storage variables, required
+DS_ACCESS_KEY=
+DS_BASEURL=
+DS_BUCKET=
+DS_HOST=
+DS_SECRET_KEY=
+
+# Rollbar variables, optional
+ROLLBAR_ENV=
+ROLLBAR_TOKEN=
+
+echo ============================== Starting Bot ==============================
+echo    Version:  ${version}
+echo Build Date:  ${timestamp}
+echo   Revision:  https://github.com/guacamoledragon/throw-voice/commit/${revision}
+echo ==========================================================================
+
+java -Xmx512m -cp ${name}-${version}.jar:lib\* tech.gdragon.App
