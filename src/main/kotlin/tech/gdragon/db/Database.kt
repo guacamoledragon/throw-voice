ea8bf531ccf1c678dbef75fadbed99dd543dd7b5
Jose V. Trigueros <j.v.trigueros@gmail.com>
2020-06-26 17:00:05 -0700

    Add postgresql driver and configure DB connection

--- src/main/kotlin/tech/gdragon/db/Database.kt
+++ src/main/kotlin/tech/gdragon/db/Database.kt
@@ -9,12 +9,14 @@ fun initializeDatabase(database: String) {
   // Ensure that Joda Time deals with time as UTC
   DateTimeZone.setDefault(DateTimeZone.UTC)
 
-  val setupConnection: (Connection) -> Unit = {
+  /*val setupConnection: (Connection) -> Unit = {
     val statement = it.createStatement()
     statement.executeUpdate("PRAGMA foreign_keys = ON")
   }
 
   Database.connect("jdbc:sqlite:$database", driver = "org.sqlite.JDBC", setupConnection = setupConnection)
   // Override default isolation level
-  TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_READ_UNCOMMITTED
+  TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_READ_UNCOMMITTED*/
+  Database.connect("jdbc:postgresql://localhost:5432/postgres", "org.postgresql.Driver" ,"postgres", "password")
+//  Database.connect("jdbc:pgsql://localhost:5432/postgres", "com.impossibl.postgres.jdbc.PGDriver" ,"postgres", "password")
 }
