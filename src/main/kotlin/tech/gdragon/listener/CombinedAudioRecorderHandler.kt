562b14e020d3a1f5eab5bc02106c5fab78a1f463
Jose V. Trigueros <j.v.trigueros@gmail.com>
2020-07-12 16:37:25 -0700

    Remove buffer from recording chain, maybe this is what's causing issues

--- src/main/kotlin/tech/gdragon/listener/CombinedAudioRecorderHandler.kt
+++ src/main/kotlin/tech/gdragon/listener/CombinedAudioRecorderHandler.kt
@@ -2,7 +2,6 @@ package tech.gdragon.listener
 
 import com.squareup.tape.QueueFile
 import de.sciss.jump3r.lowlevel.LameEncoder
-import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
@@ -32,7 +31,6 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 import java.util.*
-import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 
 class CombinedAudioRecorderHandler(var volume: Double, val voiceChannel: VoiceChannel, val defaultChannel: TextChannel) : AudioReceiveHandler, KoinComponent {
@@ -126,22 +124,17 @@ class CombinedAudioRecorderHandler(var volume: Double, val voiceChannel: VoiceCh
 
     return subject
       ?.doOnNext { isAfk(it.users.size) }
-      ?.map { it.getAudioData(volume) }
-      ?.buffer(BUFFER_TIMEOUT, TimeUnit.MILLISECONDS, BUFFER_MAX_COUNT)
-      ?.flatMap { byteArrays ->
+      ?.map {
+        val bytes = it.getAudioData(volume)
         val baos = ByteArrayOutputStream()
-
-        byteArrays.forEach {
-          if (pcmMode) {
-            baos.writeBytes(it)
-          } else {
-            val buffer = ByteArray(it.size)
-            val bytesEncoded = encoder.encodeBuffer(it, 0, it.size, buffer)
-            baos.write(buffer, 0, bytesEncoded)
-          }
+        if (pcmMode) {
+          baos.writeBytes(bytes)
+        } else {
+          val buffer = ByteArray(bytes.size)
+          val bytesEncoded = encoder.encodeBuffer(bytes, 0, bytes.size, buffer)
+          baos.write(buffer, 0, bytesEncoded)
         }
-
-        Observable.fromArray(baos.toByteArray())
+        baos.toByteArray()
       }
       ?.doOnNext {
         val percentage = recordingSize * 100 / MAX_RECORDING_SIZE
