b6168b8dec276145de6ba4cf49da9a3cf7a5ffd1
Jose V. Trigueros <j.v.trigueros@gmail.com>
2020-12-21 19:14:39 -0800

    Create Database Koin module that can toggle between embedded and remote

--- src/main/kotlin/tech/gdragon/db/Database.kt
+++ src/main/kotlin/tech/gdragon/db/Database.kt
@@ -1,35 +1,46 @@
 package tech.gdragon.db
 
 import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
-import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Database as ExposedDatabase
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTimeZone
-import org.koin.core.component.KoinComponent
 import tech.gdragon.db.table.Tables
 
-fun initializeDatabase(database: String?, hostname: String?, username: String?, password: String?) {
-  // Ensure that Joda Time deals with time as UTC
-  DateTimeZone.setDefault(DateTimeZone.UTC)
-
-  Database.connect("jdbc:postgresql://$hostname/$database", "org.postgresql.Driver", username!!, password!!)
-//  Database.connect("jdbc:pgsql://$hostname/$database", "com.impossibl.postgres.jdbc.PGDriver" ,username, password)
+interface Database {
+  fun stop()
 }
 
-class DatabaseLite : KoinComponent {
-  val pgBuilder = EmbeddedPostgres
+class EmbeddedDatabase(dataDirectory: String) : Database {
+  var pg: EmbeddedPostgres = EmbeddedPostgres
     .builder()
     .setCleanDataDirectory(false)
-    .setDataDirectory("./local-db")
+    .setDataDirectory(dataDirectory)
+    .start()
 
   init {
-    val pg = pgBuilder.start()
-
     // Source: https://stackoverflow.com/q/32336651
     val db = pg.getPostgresDatabase(mapOf("stringtype" to "unspecified"))
-    Database.connect(db::getConnection)
+    ExposedDatabase.connect(db::getConnection)
+
     transaction {
       SchemaUtils.create(*Tables.allTables)
     }
+  }
+
+  override fun stop() {
+    pg.close()
+  }
+}
+
+class RemoteDatabase(database: String?, hostname: String?, username: String?, password: String?) : Database {
+  init {
+    // Ensure that Joda Time deals with time as UTC
+    DateTimeZone.setDefault(DateTimeZone.UTC)
+    ExposedDatabase.connect("jdbc:postgresql://$hostname/$database", "org.postgresql.Driver", username!!, password!!)
+  }
+
+  override fun stop() {
+    // Do nothing...
   }
 }
