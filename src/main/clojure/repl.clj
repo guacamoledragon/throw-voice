40c05fe6880e54bdf22ac5fa317cdac3a1662864
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-28 18:19:01 -0800

    Combine the Clojure files into one, right now it's a mess :\

--- src/main/clojure/repl.clj
+++ src/main/clojure/repl.clj
@@ -1,39 +1,95 @@
+(ns repl
+  (:import (tech.gdragon.discord Bot)
+           (net.dv8tion.jda.api JDA)
+           (net.dv8tion.jda.api.entities Guild TextChannel)
+           (com.squareup.tape QueueFile QueueFile$ElementReader)
+           (java.io InputStream))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
 (use 'cl-java-introspector.core)
-(import '(net.dv8tion.jda.api.sharding DefaultShardManager))
-(def bot (get-obj "bot"))
-(def ^DefaultShardManager shard-manager (-> bot .api .getShardManager))
 
-(import '(org.koin.core.context GlobalContext)
-        '(net.dv8tion.jda.api.entities Activity)
-        '(net.dv8tion.jda.api.entities Activity$ActivityType))
+(def ^Bot bot (get-obj "bot"))
 
-;(.setProperty (.get (GlobalContext/INSTANCE)) "MAINTENANCE" "true")
+(defn get-channel
+  "Find Discord channel and return"
+  [^JDA jda guild-name channel-name]
+  (let [^Guild guild         (first (.getGuildsByName jda guild-name true))
+        ^TextChannel channel (first (.getTextChannelsByName guild channel-name true))]
+    channel))
 
-(defn which-shard
-  "Find the shard ID for a given guild ID"
-  [jda]
-  (let [shard-manager (.getShardManager jda)]))
-
-
-(def activity
-  (Activity/of
-    Activity$ActivityType/DEFAULT
-    (str "2.9.1 | https://pawa.im")
-    "https://pawa.im"))
+(defn send-message!
+  "Send a text message"
+  [^TextChannel channel message]
+  (.. channel (sendMessage message) (queue)))
 
 (comment
-  ;; Set pawa's status
-  (-> bot
-      .api
-      .getPresence
-      (.setActivity activity)))
+  (def channel (get-channel
+                 (.api bot)
+                 "Guacamole Dragon"
+                 "bot-testing")))
+
+(comment (send-message! channel "Hello World!"))
+
+(defn queue->mp3
+  "Create MP3 from Queue file"
+  [queue-filename]
+  (with-open [os (io/output-stream (io/file (str/replace queue-filename "queue" "mp3")))]
+    (let [queue-file (QueueFile. (io/file queue-filename))]
+      (.forEach queue-file (reify QueueFile$ElementReader
+                             (read [this is length]
+                               (.transferTo is os)))))))
 
 (comment
-  ())
+  (let [jda           (.api bot)
+        guild         (first (.getGuildsByName jda "Guacamole Dragon" true))
+        audio-manager (.getAudioManager guild)]
+    (.. audio-manager getConnectedChannel getName)))
 
 (comment
-  (doseq [shard (.getShards shard-manager)]
-    (print (.getShardInfo shard) (.getStatus shard) ": ")
-    (println (.getGuildById shard 408795211901173762)))
+  (use 'cl-java-introspector.core)
+  (import '(net.dv8tion.jda.api.sharding DefaultShardManager))
+  (def bot (get-obj "bot"))
+  (def ^DefaultShardManager shard-manager (-> bot .api .getShardManager))
 
-  (.restart shard-manager))
+  (import '(org.koin.core.context GlobalContext)
+          '(net.dv8tion.jda.api.entities Activity)
+          '(net.dv8tion.jda.api.entities Activity$ActivityType))
+
+  ;(.setProperty (.get (GlobalContext/INSTANCE)) "MAINTENANCE" "true")
+
+  (defn which-shard
+    "Find the shard ID for a given guild ID"
+    [jda]
+    (let [shard-manager (.getShardManager jda)]))
+
+
+  (def activity
+    (Activity/of
+      Activity$ActivityType/DEFAULT
+      (str "2.9.1 | https://pawa.im")
+      "https://pawa.im"))
+
+  (comment
+    ;; Set pawa's status
+    (-> bot
+        .api
+        .getPresence
+        (.setActivity activity)))
+
+  (comment
+    (require '[criterium.core :as crit])
+    (import '(org.jetbrains.exposed.sql.transactions ThreadLocalTransactionManagerKt)
+            '(kotlin.jvm.functions Function1)
+            '(tech.gdragon BotUtils))
+    (let [guild (.getGuildById shard-manager 333055724198559745)]
+      (crit/with-progress-reporting
+        (crit/bench
+          (.getPrefix BotUtils/INSTANCE guild)))))
+
+  (comment
+    (doseq [shard (.getShards shard-manager)]
+      (print (.getShardInfo shard) (.getStatus shard) ": ")
+      (println (.getGuildById shard 408795211901173762)))
+
+    (.restart shard-manager)))
