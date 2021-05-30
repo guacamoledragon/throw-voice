5662b647d5f909564dc25eefd87ee8d0c4a137b1
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-05-30 10:48:14 -0700

    Add missing class match in valueFromDB

--- src/main/kotlin/tech/gdragon/db/LanguageColumnType.kt
+++ src/main/kotlin/tech/gdragon/db/LanguageColumnType.kt
@@ -5,6 +5,7 @@ import tech.gdragon.i18n.Lang
 
 class LanguageColumnType : TextColumnType() {
   override fun valueFromDB(value: Any): Any = when (value) {
+    is java.sql.Clob -> Lang.valueOf(value.characterStream.readText())
     is ByteArray -> Lang.valueOf(String(value))
     is Lang -> value
     else -> Lang.valueOf(value as String)
