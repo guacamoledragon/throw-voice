42bd90ad5dfbb71f8c56ab85ace5c089468f783d
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-09-18 16:09:47 -0700

    Add shutdown fn to Database

--- src/main/kotlin/tech/gdragon/db/Database.kt
+++ src/main/kotlin/tech/gdragon/db/Database.kt
@@ -2,13 +2,22 @@ package tech.gdragon.db
 
 import mu.KotlinLogging
 import org.flywaydb.core.Flyway
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.joda.time.DateTimeZone
 import org.jetbrains.exposed.sql.Database as ExposedDatabase
 
-interface Database
+interface Database {
+  val database: ExposedDatabase?
+
+  fun shutdown() {
+    database?.let(TransactionManager::closeAndUnregister)
+  }
+}
 
 class EmbeddedDatabase(dataDirectory: String) : Database {
   val logger = KotlinLogging.logger { }
+  private var _database: ExposedDatabase? = null
+  override val database = _database
 
   init {
     val url = "jdbc:h2:file:$dataDirectory/settings.db"
@@ -30,14 +39,18 @@ class EmbeddedDatabase(dataDirectory: String) : Database {
         }
       }
 
-    ExposedDatabase.connect(url, "org.h2.Driver")
+    _database = ExposedDatabase.connect(url, "org.h2.Driver")
   }
 }
 
 class RemoteDatabase(database: String?, hostname: String?, username: String?, password: String?) : Database {
+  private var _database: ExposedDatabase? = null
+  override val database = _database
+
   init {
     // Ensure that Joda Time deals with time as UTC
     DateTimeZone.setDefault(DateTimeZone.UTC)
-    ExposedDatabase.connect("jdbc:postgresql://$hostname/$database", "org.postgresql.Driver", username!!, password!!)
+    _database = ExposedDatabase.connect("jdbc:postgresql://$hostname/$database", "org.postgresql.Driver", username!!, password!!)
   }
+
 }
