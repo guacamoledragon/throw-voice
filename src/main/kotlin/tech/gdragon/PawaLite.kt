afd7285f9f6f793ea9ae19ee7f59a4c2834ae0d5
Jose V. Trigueros <jvtrigueros@KOS-MOS.localdomain>
2021-11-21 00:04:56 -0800

    Add PawaLite playground

---
+++ src/main/kotlin/tech/gdragon/PawaLite.kt
@@ -0,0 +1,11 @@
+package tech.gdragon
+
+import org.koin.java.KoinJavaComponent
+import tech.gdragon.discord.Bot
+
+fun main() {
+  val app = App.start()
+  val jda = KoinJavaComponent.get<Bot>(Bot::class.java).api()
+  val inviteUrl = jda.setRequiredScopes("applications.commands").getInviteUrl(Bot.PERMISSIONS)
+  println("inviteUrl = $inviteUrl")
+}
