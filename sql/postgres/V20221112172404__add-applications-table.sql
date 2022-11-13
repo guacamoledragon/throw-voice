64749dfba5cd7dcde89b8196d6427d4ade11bd42
Jose V. Trigueros <j.v.trigueros@gmail.com>
2022-11-13 06:13:05 +0000

    Add db migration to add Applications table
    This table will store the Discord Application ID. For now, nothing else
    will be done with this, but will be used in the future as a join table
    so that many bot instances can use the same database.

---
+++ sql/postgres/V20221112172404__add-applications-table.sql
@@ -0,0 +1,14 @@
+create table applications
+(
+  id         bigint,
+  created_on timestamptz
+);
+
+comment on table applications is 'Allows multiple bot instances to use the same database.';
+
+comment on column applications.id is 'Discord Application ID';
+
+comment on column applications.created_on is 'When this entry was created.';
+
+create unique index applications_id_uindex
+  on applications (id);
