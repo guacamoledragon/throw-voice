d74f8db6dafaaf919951b3b1c4c90fbcca938822
Jose V. Trigueros <j.v.trigueros@gmail.com>
2022-12-13 02:38:45 +0000

    Refactor Stop command to support Slash command

--- src/main/kotlin/tech/gdragon/commands/audio/Stop.kt
+++ src/main/kotlin/tech/gdragon/commands/audio/Stop.kt
@@ -1,37 +1,61 @@
 package tech.gdragon.commands.audio
 
+import dev.minn.jda.ktx.CoroutineEventListener
+import dev.minn.jda.ktx.interactions.Command
+import net.dv8tion.jda.api.entities.TextChannel
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
-import org.jetbrains.exposed.sql.transactions.transaction
 import tech.gdragon.BotUtils
 import tech.gdragon.api.pawa.Pawa
 import tech.gdragon.commands.CommandHandler
 import tech.gdragon.commands.InvalidCommand
-import tech.gdragon.db.dao.Guild
 import tech.gdragon.i18n.Babel
 import tech.gdragon.i18n.Lang
+import net.dv8tion.jda.api.entities.Guild as DiscordGuild
+import tech.gdragon.i18n.Stop as StopTranslator
 
 class Stop : CommandHandler() {
+  companion object {
+    val command = Command("stop", "Stop recording voice channel.")
+
+    fun handler(pawa: Pawa, guild: DiscordGuild, textChannel: TextChannel?): String {
+      val translator: StopTranslator = pawa.translator(guild.idLong)
+      return if (guild.audioManager.isConnected) {
+        val voiceChannel = guild.audioManager.connectedChannel!!
+        val save = pawa.autoSave(guild.idLong)
+        val recorder = BotUtils.leaveVoiceChannel(voiceChannel, textChannel, save)
+        pawa.stopRecording(recorder.session)
+        ":wave: _${translator.leaveChannel(voiceChannel.id)}_"
+      } else {
+        ":no_entry_sign: _${translator.noChannel}_"
+      }
+    }
+
+    fun slashHandler(pawa: Pawa): suspend CoroutineEventListener.(SlashCommandEvent) -> Unit = { event ->
+      tracer.sendEvent(mapOf("command" to command.name))
+
+      event.guild?.let {
+        val textChannel = try {
+          event.textChannel
+        } catch (_: Exception) {
+          // This will happen if the event is triggered from a Voice Channel chat
+          // Source: https://support.discord.com/hc/en-us/articles/4412085582359-Text-Channels-Text-Chat-In-Voice-Channels#h_01FMJT3SP072ZFJCZWR0EW6CJ1
+          BotUtils.defaultTextChannel(it) ?: BotUtils.findPublicChannel(it)
+        }
+
+        val message = handler(pawa, it, textChannel)
+        event.reply(message).queue()
+      }
+    }
+  }
+
   override fun action(args: Array<String>, event: GuildMessageReceivedEvent, pawa: Pawa) {
     require(args.isEmpty()) {
       throw InvalidCommand(::usage, "Incorrect number of arguments: ${args.size}")
     }
 
-    val translator = transaction { Guild[event.guild.idLong].settings.language.let(Babel::stop) }
-    val defaultChannel = BotUtils.defaultTextChannel(event.guild) ?: event.channel
-
-    val message =
-      if (event.guild.audioManager.isConnected) {
-        event.guild.audioManager.connectedChannel?.let {
-          val save = BotUtils.autoSave(event.guild)
-          BotUtils.leaveVoiceChannel(it, defaultChannel, save)
-          ":wave: _${translator.leaveChannel(it.id)}_"
-        } ?: ":no_entry_sign: _${translator.noChannel}_"
-
-      } else {
-        ":no_entry_sign: _${translator.noChannel}_"
-      }
-
-    BotUtils.sendMessage(defaultChannel, message)
+    val message = handler(pawa, event.guild, event.channel)
+    BotUtils.sendMessage(event.channel, message)
   }
 
   override fun usage(prefix: String, lang: Lang): String {
