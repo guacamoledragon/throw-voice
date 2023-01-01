aa9da249ef59d548bc33ff104aa575ae0b70b81d
Jose V. Trigueros <j.v.trigueros@gmail.com>
2023-01-01 20:02:04 +0000

    Update ShutdownEvent imoprt

--- src/main/kotlin/tech/gdragon/listener/SystemEventListener.kt
+++ src/main/kotlin/tech/gdragon/listener/SystemEventListener.kt
@@ -1,10 +1,10 @@
 package tech.gdragon.listener
 
 import mu.KotlinLogging
-import net.dv8tion.jda.api.events.ShutdownEvent
+import net.dv8tion.jda.api.events.session.ShutdownEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-class SystemEventListener: ListenerAdapter() {
+class SystemEventListener : ListenerAdapter() {
   private val logger = KotlinLogging.logger { }
 
   override fun onShutdown(event: ShutdownEvent) {
