e18c4759eeec4e657972c922799ba9d7de3b07a6
Jose V. Trigueros <j.v.trigueros@gmail.com>
2023-02-04 19:48:41 +0000

    Update PawaTest

--- src/test/kotlin/tech/gdragon/api/pawa/PawaTest.kt
+++ src/test/kotlin/tech/gdragon/api/pawa/PawaTest.kt
@@ -1,53 +1,61 @@
 package tech.gdragon.api.pawa
 
-import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import org.jetbrains.exposed.sql.transactions.transaction
-import tech.gdragon.db.EmbeddedDatabase
+import org.koin.core.context.startKoin
+import tech.gdragon.db.Database
 import tech.gdragon.db.dao.Guild
+import tech.gdragon.db.databaseModule
 import tech.gdragon.discord.Command
 
-class PawaTest : FunSpec() {
-  private lateinit var db: EmbeddedDatabase
-  private lateinit var pawa: Pawa
+class PawaTest : FunSpec({
+  lateinit var pawa: Pawa
 
-  private val guildId = 1L
+  val guildId = 1L
 
-  init {
-    context("when alias") {
-      test("doesn't exist, it should be created") {
-        val alias = pawa.createAlias(guildId, Command.RECORD, "r")
-        alias.shouldNotBeNull()
-        alias.alias.shouldBe("r")
-        alias.name.shouldBe("RECORD")
-      }
+  context("when alias") {
+    test("doesn't exist, it should be created") {
+      val alias = pawa.createAlias(guildId, Command.RECORD, "r")
+      alias.shouldNotBeNull()
+      alias.alias.shouldBe("r")
+      alias.name.shouldBe("RECORD")
+    }
 
-      test("exists, it shouldn't be created") {
-        val alias = pawa.createAlias(guildId, Command.RECORD, "r")
-        alias.shouldNotBeNull()
-      }
+    test("exists, it shouldn't be created") {
+      val alias = pawa.createAlias(guildId, Command.RECORD, "r")
+      alias.shouldNotBeNull()
+    }
 
-      test("command is \"alias\" it shouldn't be created") {
-        val alias = pawa.createAlias(guildId, Command.ALIAS, "a")
-        alias.shouldBeNull()
-      }
+    test("command is \"alias\" it shouldn't be created") {
+      val alias = pawa.createAlias(guildId, Command.ALIAS, "a")
+      alias.shouldBeNull()
+    }
 
-      test("is a command name, it shouldn't be created") {
-        val alias = pawa.createAlias(guildId, Command.RECORD, "record")
-        alias.shouldBeNull()
-      }
+    test("is a command name, it shouldn't be created") {
+      val alias = pawa.createAlias(guildId, Command.RECORD, "record")
+      alias.shouldBeNull()
     }
   }
 
-  override suspend fun beforeSpec(spec: Spec) {
-    db = EmbeddedDatabase("./target")
+  beforeSpec {
+    val app = startKoin {
+      koin.apply{
+        setProperty("BOT_STANDALONE", "true")
+        setProperty("BOT_DATA_DIR", "./target")
+      }
+      koin.loadModules(listOf(databaseModule))
+    }
+    val db: Database = app.koin.get()
     pawa = Pawa(1L, db, false)
     transaction(db.database) {
       Guild.findOrCreate(guildId, "Test Guild")
     }
-    super.beforeSpec(spec)
   }
-}
+
+  afterSpec {
+    pawa.db.shutdown()
+  }
+})
