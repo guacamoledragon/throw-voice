d741809c7f60e1381213aa8bca70b716131470f7
Jose V. Trigueros <j.v.trigueros@gmail.com>
2023-02-23 04:47:50 +0000

    Include Pawa App recording URL and format messages

--- src/main/kotlin/tech/gdragon/commands/slash/Recover.kt
+++ src/main/kotlin/tech/gdragon/commands/slash/Recover.kt
@@ -9,6 +9,7 @@ import dev.minn.jda.ktx.interactions.components.link
 import dev.minn.jda.ktx.interactions.components.row
 import dev.minn.jda.ktx.messages.Embed
 import dev.minn.jda.ktx.messages.MessageCreate
+import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent
 import org.apache.commons.io.FileUtils
 import org.koin.core.context.GlobalContext
@@ -16,40 +17,47 @@ import tech.gdragon.BotUtils.trigoman
 import tech.gdragon.api.pawa.Pawa
 import tech.gdragon.data.Datastore
 import tech.gdragon.db.dao.Recording
+import tech.gdragon.db.table.Tables
+import tech.gdragon.discord.message.ErrorEmbed
 import tech.gdragon.discord.message.RequestAccessModal
 import tech.gdragon.koin.getStringProperty
 import java.awt.Color
 
-class RecordingReply(recording: Recording) {
+class RecordingReply(recording: Recording, appBaseUrl: String) {
+  private val guildId = recording.readValues[Tables.Recordings.guild].value
+  private val sessionId = recording.id.value
+  private val createdOn = recording.createdOn.toString()
+  private val duration = "${recording.pseudoDuration().toMinutes()} minutes"
+  private val size = FileUtils.byteCountToDisplaySize(recording.size)
+  private val appRecordingUrl = "$appBaseUrl/v1/recordings?guild=$guildId&session-id=$sessionId"
+  private val voteUrl = "https://top.gg/bot/pawa/vote"
+
   val embed = Embed {
-    title = recording.id.value
-    description = "Attempted to recover recording, and this is what we found."
+    title = sessionId
+    description = "Here's your recording, enjoy!"
     color = Color.decode("#596800").rgb
+    url = recording.url
     field {
       name = "Created On"
-      value = recording.createdOn.toString()
+      value = createdOn
       inline = false
     }
     field {
       name = "Duration"
-      value = "${recording.pseudoDuration().toMinutes()} minutes"
+      value = duration
       inline = true
     }
     field {
       name = "Size"
-      value = FileUtils.byteCountToDisplaySize(recording.size)
-    }
-    field {
-      name = "URL"
-      value = recording.url!!
-      inline = false
+      value = size
     }
   }
 
   val message = MessageCreate {
     embeds += embed
     components += row(
-      link(recording.url!!, label = "Download"),
+      link(appRecordingUrl, label = "View Recording"),
+      link(voteUrl, label = "Vote", emoji = Emoji.fromUnicode("❤"))
     )
   }
 }
@@ -68,21 +76,31 @@ object Recover {
 
     val sessionId = event.getOption<String>("session-id")!!
 
-    val datastore = GlobalContext.get().get<Datastore>()
-    val dataDirectory = GlobalContext.get().getStringProperty("BOT_DATA_DIR")
+    val koin = GlobalContext.get()
+    val datastore = koin.get<Datastore>()
+    val dataDirectory = koin.getStringProperty("BOT_DATA_DIR")
+    val appUrl = koin.getStringProperty("APP_URL")
 
+    val guildId = event.guild!!.idLong
+
+    // Reply to the user, the upcoming request requires database interaction
     event.deferReply().queue()
-    val recording = pawa.recoverRecording(dataDirectory, datastore, event.guild?.idLong!!, sessionId)
+    val recording = pawa.recoverRecording(dataDirectory, datastore, guildId, sessionId)
 
-    if (recording == null) {
-      event.hook.sendMessage("Couldn't restore recording").queue()
-    } else {
-      val embed = RecordingReply(recording)
+    val interaction =
+      if (recording == null) {
+        val errorEmbed = ErrorEmbed("Recording Not Found", "Couldn't find recording with Session ID: $sessionId")
+        event
+          .hook
+          .sendMessage(errorEmbed.message)
+      } else {
+        val embed = RecordingReply(recording, appUrl)
 
-      event
-        .hook
-        .sendMessage(embed.message)
-        .queue()
-    }
+        event
+          .hook
+          .sendMessage(embed.message)
+      }
+
+    interaction.queue()
   }
 }
