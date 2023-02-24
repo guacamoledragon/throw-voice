41875f6b26baf5b94a5909844c8795628b8c0202
Jose V. Trigueros <j.v.trigueros@gmail.com>
2023-02-24 17:33:05 +0000

    Move RecordingReply to a different namespace

--- src/main/kotlin/tech/gdragon/commands/slash/Recover.kt
+++ src/main/kotlin/tech/gdragon/commands/slash/Recover.kt
@@ -5,62 +5,15 @@ import dev.minn.jda.ktx.interactions.commands.Command
 import dev.minn.jda.ktx.interactions.commands.option
 import dev.minn.jda.ktx.interactions.commands.restrict
 import dev.minn.jda.ktx.interactions.components.getOption
-import dev.minn.jda.ktx.interactions.components.link
-import dev.minn.jda.ktx.interactions.components.row
-import dev.minn.jda.ktx.messages.Embed
-import dev.minn.jda.ktx.messages.MessageCreate
-import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent
-import org.apache.commons.io.FileUtils
 import org.koin.core.context.GlobalContext
 import tech.gdragon.BotUtils.trigoman
 import tech.gdragon.api.pawa.Pawa
 import tech.gdragon.data.Datastore
-import tech.gdragon.db.dao.Recording
-import tech.gdragon.db.table.Tables
 import tech.gdragon.discord.message.ErrorEmbed
+import tech.gdragon.discord.message.RecordingReply
 import tech.gdragon.discord.message.RequestAccessModal
 import tech.gdragon.koin.getStringProperty
-import java.awt.Color
-
-class RecordingReply(recording: Recording, appBaseUrl: String) {
-  private val guildId = recording.readValues[Tables.Recordings.guild].value
-  private val sessionId = recording.id.value
-  private val createdOn = recording.createdOn.toString()
-  private val duration = "${recording.pseudoDuration().toMinutes()} minutes"
-  private val size = FileUtils.byteCountToDisplaySize(recording.size)
-  private val appRecordingUrl = "$appBaseUrl/v1/recordings?guild=$guildId&session-id=$sessionId"
-  private val voteUrl = "https://top.gg/bot/pawa/vote"
-
-  val embed = Embed {
-    title = sessionId
-    description = "Here's your recording, enjoy!"
-    color = Color.decode("#596800").rgb
-    url = recording.url
-    field {
-      name = "Created On"
-      value = createdOn
-      inline = false
-    }
-    field {
-      name = "Duration"
-      value = duration
-      inline = true
-    }
-    field {
-      name = "Size"
-      value = size
-    }
-  }
-
-  val message = MessageCreate {
-    embeds += embed
-    components += row(
-      link(appRecordingUrl, label = "View Recording"),
-      link(voteUrl, label = "Vote", emoji = Emoji.fromUnicode("❤"))
-    )
-  }
-}
 
 object Recover {
   val command = Command("recover", "Recover a recording that failed to upload.") {
---
+++ src/main/kotlin/tech/gdragon/discord/message/RecordingReply.kt
@@ -0,0 +1,50 @@
+package tech.gdragon.discord.message
+
+import dev.minn.jda.ktx.interactions.components.link
+import dev.minn.jda.ktx.interactions.components.row
+import dev.minn.jda.ktx.messages.Embed
+import dev.minn.jda.ktx.messages.MessageCreate
+import net.dv8tion.jda.api.entities.emoji.Emoji
+import org.apache.commons.io.FileUtils
+import tech.gdragon.db.dao.Recording
+import tech.gdragon.db.table.Tables
+import java.awt.Color
+
+class RecordingReply(recording: Recording, appBaseUrl: String) {
+  private val guildId = recording.readValues[Tables.Recordings.guild].value
+  private val sessionId = recording.id.value
+  private val createdOn = recording.createdOn.toString()
+  private val duration = "${recording.pseudoDuration().toMinutes()} minutes"
+  private val size = FileUtils.byteCountToDisplaySize(recording.size)
+  private val appRecordingUrl = "$appBaseUrl/v1/recordings?guild=$guildId&session-id=$sessionId"
+  private val voteUrl = "https://top.gg/bot/pawa/vote"
+
+  val embed = Embed {
+      title = sessionId
+      description = "Here's your recording, enjoy!"
+      color = Color.decode("#596800").rgb
+      url = recording.url
+      field {
+          name = "Created On"
+          value = createdOn
+          inline = false
+      }
+      field {
+          name = "Duration"
+          value = duration
+          inline = true
+      }
+      field {
+          name = "Size"
+          value = size
+      }
+  }
+
+  val message = MessageCreate {
+      embeds += embed
+      components += row(
+          link(appRecordingUrl, label = "View Recording"),
+          link(voteUrl, label = "Vote", emoji = Emoji.fromUnicode("❤"))
+      )
+  }
+}
