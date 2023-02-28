9b1a6a77b3697c9061f4633ead953fa75c68614e
Jose V. Trigueros <j.v.trigueros@gmail.com>
2023-02-28 07:05:04 +0000

    Add appUrl to PawaConfig
    Will use with the default of https://app.pawa.im for now.

--- src/main/kotlin/tech/gdragon/api/pawa/PawaConfig.kt
+++ src/main/kotlin/tech/gdragon/api/pawa/PawaConfig.kt
@@ -1,12 +1,22 @@
 package tech.gdragon.api.pawa
 
-class PawaConfig private constructor() {
-  class Builder()
+class PawaConfig private constructor(
+  val appUrl: String,
+) {
+
+  class Builder(
+    /**
+     * The URL of the pawa recordings app. Defaults to [https://app.pawa.im](https://app.pawa.im)
+     */
+    var appUrl: String? = null
+  )
 
   companion object {
     operator fun invoke(body: Builder.() -> Unit = {}): PawaConfig {
       val builder = Builder().apply(body)
-      return PawaConfig()
+      return PawaConfig(
+        appUrl = builder.appUrl ?: "https://app.pawa.im"
+      )
     }
   }
 }
