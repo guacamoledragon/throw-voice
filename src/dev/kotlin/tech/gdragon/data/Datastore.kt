71c50a5e86737f6638f1915ec2789afa8df75698
Jose V. Trigueros <j.v.trigueros@gmail.com>
2023-09-17 14:03:46 -0700

    Add testing harness for Datastore

---
+++ src/dev/kotlin/tech/gdragon/data/Datastore.kt
@@ -0,0 +1,36 @@
+package tech.gdragon.data
+
+import org.koin.core.context.startKoin
+import tech.gdragon.koin.overrideFileProperties
+import org.koin.dsl.module
+import tech.gdragon.data.Datastore
+import tech.gdragon.data.S3Datastore
+
+fun main() {
+  val app = startKoin {
+    overrideFileProperties("dev-b2.properties")
+    modules(
+      module {
+      single<Datastore>(createdAtStart = true) {
+        val bucketName = getProperty<String>("DS_BUCKET")
+        val accessKey = getProperty<String>("DS_ACCESS_KEY")
+        val secretKey = getProperty<String>("DS_SECRET_KEY")
+        val endpoint = getProperty<String>("DS_HOST")
+        val region = getProperty<String>("DS_REGION")
+        val baseUrl = getProperty<String>("DS_BASEURL")
+        S3Datastore(
+          accessKey,
+          secretKey,
+          endpoint,
+          region,
+          bucketName,
+          baseUrl
+        )
+      }
+    })
+  }
+
+  val datastore = app.koin.get<Datastore>() as S3Datastore
+  datastore.client.close()
+  app.close()
+}
