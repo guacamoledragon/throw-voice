8185b7e485c08e91852213ba56a78fba598893c6
Jose V. Trigueros <j.v.trigueros@gmail.com>
2023-09-17 12:27:51 -0700

    Remove RemoteDatastore and minio pom dependency
    Will be using the S3 API whenever we need to talk to a Minio datastore.
    We are only using it in dev so this change will reduce our dependencies
    and code.
    
    Perhaps this is something that could be added as an option for
    PawaLite.

--- src/main/kotlin/tech/gdragon/data/Datastore.kt
+++ src/main/kotlin/tech/gdragon/data/Datastore.kt
@@ -8,13 +8,11 @@ import aws.sdk.kotlin.services.s3.putObject
 import aws.smithy.kotlin.runtime.content.ByteStream
 import aws.smithy.kotlin.runtime.content.fromFile
 import aws.smithy.kotlin.runtime.net.Url
-import io.minio.*
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import net.jodah.failsafe.Failsafe
 import net.jodah.failsafe.RetryPolicy
 import org.apache.commons.io.FileUtils
-import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.FileInputStream
 import java.nio.file.Files
@@ -43,69 +41,6 @@ class LocalDatastore(val localBucket: String) : Datastore {
       Files.copy(it, newFile)
       return UploadResult.from(newFile)
     }
-  }
-}
-
-@Deprecated("Use S3Datastore instead.")
-class RemoteDatastore(
-  accessKey: String,
-  val bucketName: String,
-  endpoint: String,
-  secretKey: String,
-  val baseUrl: String
-) : Datastore {
-  val logger = KotlinLogging.logger { }
-
-  private val client: MinioClient = MinioClient
-    .builder()
-    .endpoint(endpoint)
-    .credentials(accessKey, secretKey)
-    .build()
-
-  private val retryPolicy: RetryPolicy<Unit> = RetryPolicy<Unit>()
-    .withBackoff(2, 30, ChronoUnit.SECONDS)
-    .withJitter(.25)
-    .onRetry { ex -> logger.warn { "Failure #${ex.attemptCount}. Retrying!" } }
-
-  init {
-    if (!client.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
-      logger.warn {
-        "$bucketName bucket does not exist! Creating..."
-      }
-      client.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build())
-    }
-  }
-
-  override fun upload(key: String, file: File): UploadResult {
-    logger.info {
-      "Uploading: $baseUrl/$key"
-    }
-
-    Failsafe.with(retryPolicy).run { ->
-      ByteArrayInputStream(file.readBytes()).use { bais ->
-        val putObjectArgs = PutObjectArgs
-          .builder()
-          .contentType("audio/mpeg")
-          .bucket(bucketName)
-          .`object`(key)
-          .stream(bais, bais.available().toLong(), -1)
-          .build()
-        client.putObject(putObjectArgs)
-      }
-    }
-
-    val statObjectArgs = StatObjectArgs
-      .builder()
-      .bucket(bucketName)
-      .`object`(key)
-      .build()
-    val stat = UploadResult.from(baseUrl, client.statObject(statObjectArgs))
-
-    logger.info {
-      "Finished uploading file - (${FileUtils.byteCountToDisplaySize(stat.size)}) ${stat.key}"
-    }
-
-    return stat
   }
 }
 
@@ -187,13 +122,6 @@ class S3Datastore(
 
 data class UploadResult(val key: String, val timestamp: Instant, val size: Long, val url: String) {
   companion object {
-    fun from(baseUrl: String, stat: StatObjectResponse): UploadResult {
-      val createdTime = stat.lastModified().toInstant()
-      stat.lastModified().toInstant()
-
-      return UploadResult(stat.`object`(), createdTime, stat.size(), "$baseUrl/${stat.`object`()}")
-    }
-
     fun from(path: Path, url: String = path.toUri().toString()): UploadResult {
       val attributes = Files.readAttributes(path, BasicFileAttributes::class.java)
       val createdTime = attributes.creationTime().toInstant()
