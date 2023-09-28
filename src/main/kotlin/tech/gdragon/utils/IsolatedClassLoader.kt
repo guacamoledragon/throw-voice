0b9ec0a493b83d6116c7b60b74603cda4d924e2b
Jose V. Trigueros <j.v.trigueros@gmail.com>
2023-09-27 18:23:54 -0700

    Add IsolatedClassLoader utility class
    This will help us load JDBC drivers at runtime without it conflicting
    with the existing classloader and namespace.

--- src/main/kotlin/tech/gdragon/utils/IsolatedClassLoader.kt
+++ src/main/kotlin/tech/gdragon/utils/IsolatedClassLoader.kt
@@ -1,2 +1,19 @@
-package tech.gdragon.utils 
+package tech.gdragon.utils
 
+import java.net.URL
+import java.net.URLClassLoader
+
+class IsolatedClassLoader(urls: Array<out URL>?) : URLClassLoader(urls) {
+  @Throws(ClassNotFoundException::class)
+  override fun loadClass(name: String): Class<*> {
+    // Check for already loaded classes first
+    return findLoadedClass(name)
+      ?: try {
+        // Try to load it locally first
+        findClass(name)
+      } catch (e: ClassNotFoundException) {
+        // Defer to the parent classloader
+        super.loadClass(name)
+      }
+  }
+}
