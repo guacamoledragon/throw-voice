a0cd2f06d62ca9b83717452b9f89e1f4ce17fe98
Jose V. Trigueros <j.v.trigueros@gmail.com>
2024-01-07 17:49:50 -0800

    Remove honeycomb usage and replace with NoOpTracer

--- src/main/kotlin/tech/gdragon/metrics/EventTracer.kt
+++ src/main/kotlin/tech/gdragon/metrics/EventTracer.kt
@@ -1,38 +1,12 @@
 package tech.gdragon.metrics
 
-import io.honeycomb.libhoney.HoneyClient
-import io.honeycomb.libhoney.LibHoney.create
-import io.honeycomb.libhoney.LibHoney.options
-
 interface EventTracer {
   fun sendEvent(fields: Map<String, Any>)
 
   fun shutdown()
 }
 
-class Honey(apiKey: String) : EventTracer {
-  private val honeyClient: HoneyClient = create(
-    options()
-      .setWriteKey(apiKey)
-      .setDataset("pawa")
-      .setSampleRate(1)
-      .build()
-  )
-
-  override fun sendEvent(fields: Map<String, Any>) {
-    honeyClient
-      .createEvent()
-      .addFields(fields)
-      .setTimestamp(System.currentTimeMillis())
-      .send()
-  }
-
-  override fun shutdown() {
-    honeyClient.closeOnShutdown()
-  }
-}
-
-class NoHoney() : EventTracer {
+class NoOpTracer() : EventTracer {
   override fun sendEvent(fields: Map<String, Any>) {
     // No Op
   }
