51948047b3ab96ff3c7797fd2ac10f9f01fb46d7
Jose V. Trigueros <j.v.trigueros@gmail.com>
2024-02-04 23:02:29 -0800

    Add Scope.getBooleanProperty extension
    This allows us to use this method in the single<>{} DSL

--- src/main/kotlin/tech/gdragon/koin/KoinEx.kt
+++ src/main/kotlin/tech/gdragon/koin/KoinEx.kt
@@ -2,9 +2,12 @@ package tech.gdragon.koin
 
 import org.koin.core.Koin
 import org.koin.core.error.MissingPropertyException
+import org.koin.core.scope.Scope
 
 fun Koin.getStringProperty(key: String): String = getProperty(key)
   ?: throw MissingPropertyException("Property '$key' not found")
 
 fun Koin.getBooleanProperty(key: String): Boolean = getProperty<String>(key)?.toBoolean()
   ?: throw MissingPropertyException("Property '$key' not found")
+
+fun Scope.getBooleanProperty(key: String): Boolean = getProperty<String>(key).toBoolean()
